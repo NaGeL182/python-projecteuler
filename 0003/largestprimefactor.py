8eba713c92fa020a138e5cde148daf9827178a5b
NaGeL182 <nagel182@gmail.com>
2018-02-03 19:33:15 +0100

    solved problem 3

--- 0003/largestprimefactor.py
+++ 0003/largestprimefactor.py
@@ -2,28 +2,49 @@
 import math
 
 
+def isPrime(number):
+    """Returns True if n is prime.
+        https://stackoverflow.com/a/1801446
+    """
+    if number == 2:
+        return True
+    if number == 3:
+        return True
+    if number % 2 == 0:
+        return False
+    if number % 3 == 0:
+        return False
+
+    i = 5
+    w = 2
+
+    while i * i <= number:
+        if number % i == 0:
+            return False
+
+        i += w
+        w = 6 - w
+
+    return True
+
+
 def primeGenerator(stop):
-    number = 2
-    primes = [2]
-    while True:
-        isPrime = True
-        for i in range(primes[0], number):
-            if number % i == 0:
-                isPrime = False
-                continue
-        else:
-            if isPrime:
-                yield number
-                primes.append(number)
-            if number == stop:
-                return
-            number += 1
+    yield 2
+    number = 3
+    while number <= stop:
+        if isPrime(number):
+            print(
+                    "curent PrimeNumber: {}".format(
+                            number
+                        )
+                    )
+            yield number
+        number += 2
 
 
 def getPrimeFactors(number):
     factors = []
     for prime in primeGenerator(int(math.ceil(math.sqrt(number)))):
-        print(prime)
         if number % prime == 0:
             factors.append(prime)
     return factors
@@ -34,6 +55,30 @@ print(getPrimeFactors(input))
 
 
 # old one dont use!
+def primeGeneratorOld(stop):
+    number = 3
+    primes = [2]
+    while True:
+        isPrime = True
+        for i in range(primes[0], number):
+            if number % i == 0:
+                isPrime = False
+                continue
+        else:
+            if isPrime:
+                print(
+                    "primes size: {} curent PrimeNumber: {}".format(
+                            len(primes) + 1,
+                            number
+                        )
+                    )
+                yield number
+                primes.append(number)
+            if number == stop:
+                return
+            number += 2
+
+
 # Memmory hog likea mofo!
 # will kill the comp
 def getPrimeNumbers(start, number):
