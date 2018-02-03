c9336241a93a80aae603d93f5d77ef5f9effda15
NaGeL182 <nagel182@gmail.com>
2018-02-03 18:47:13 +0100

    implementation 2

--- 0003/largestprimefactor.py
+++ 0003/largestprimefactor.py
@@ -2,6 +2,40 @@
 import math
 
 
+def primeGenerator(stop):
+    number = 2
+    primes = [2]
+    while True:
+        isPrime = True
+        for i in range(primes[0], number):
+            if number % i == 0:
+                isPrime = False
+                continue
+        else:
+            if isPrime:
+                yield number
+                primes.append(number)
+            if number == stop:
+                return
+            number += 1
+
+
+def getPrimeFactors(number):
+    factors = []
+    for prime in primeGenerator(int(math.ceil(math.sqrt(number)))):
+        print(prime)
+        if number % prime == 0:
+            factors.append(prime)
+    return factors
+
+
+input = 600851475143
+print(getPrimeFactors(input))
+
+
+# old one dont use!
+# Memmory hog likea mofo!
+# will kill the comp
 def getPrimeNumbers(start, number):
     array = [True for x in range(number)]
     i = start
@@ -22,14 +56,10 @@ def getPrimeNumbers(start, number):
             if val and index not in [0, 1]]
 
 
-def getPrimeFactors(number):
+def getPrimeFactorsOld(number):
     factors = []
     primeNumbers = getPrimeNumbers(2, number)
     for prime in primeNumbers:
         if number % prime == 0:
             factors.append(prime)
     return factors
-
-
-input = 50000000
-print(getPrimeFactors(input))
